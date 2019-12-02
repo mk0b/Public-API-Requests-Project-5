2e4fbf25d8edc536d5123b226b96663d4892778f
Megan Katherine O'Brien <megankob@gmail.com>
2019-12-02 16:02:49 -0500

    createGallery is working.

--- JS/app.js
+++ JS/app.js
@@ -17,19 +17,6 @@ Treehouse Project 5 - Public API Requests - by TAP Student Megan Katherine O'Bri
     Global Variables
 */
 
-const RandomUsersCall = 'https://randomuser.me/api/?results=12&inc=name,location,email,dob,cell,picture&nat=us'
-
-/*
-    Helper Functions
-*/
-
-const requestData = (url) => {
-    fetch(url)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(error => console.log('There was a problem.', error));
-};
-
 //get 12 random users in one request from the randomUser API
 /*
     Info I need to capture:
@@ -42,8 +29,58 @@ const requestData = (url) => {
         7. Detailed address including street name and number, state or country, and post code.
         8. Birthday
 */
+const RandomUsersCall = 'https://randomuser.me/api/?results=12&inc=name,location,email,dob,cell,picture&nat=us';
 
-requestData(RandomUsersCall);
+//TODO: Use interpolation to add info from api call.
+const searchHTML = `<form action="#" method="get">
+<input type="search" id="search-input" class="search-input" placeholder="Search...">
+<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+</form>`;
+
+
+
+const modalHTML = `<div class="modal-container">
+<div class="modal">
+    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+    <div class="modal-info-container">
+        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+        <h3 id="name" class="modal-name cap">name</h3>
+        <p class="modal-text">email</p>
+        <p class="modal-text cap">city</p>
+        <hr>
+        <p class="modal-text">(555) 555-5555</p>
+        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+        <p class="modal-text">Birthday: 10/21/2015</p>
+    </div>
+</div>`;
+
+const modalButtonsHTML = `<div class="modal-btn-container">
+<button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+<button type="button" id="modal-next" class="modal-next btn">Next</button>
+</div>
+</div>`;
+
+
+
+/*
+    Helper Functions
+*/
+
+//Funtion for requesting data with the Fetch API, logging the response, converting to json, and catching errors.
+const requestData = (url) => {
+    return fetch(url)
+        .then(response => response.json())
+        //.then(response => console.log(response))
+        .catch(error => console.log('There was a problem.', error));
+};
+
+//Function to grab a single element, append it, and apply inner html.
+const grabNappend = (grabElement, appendElement, html) => {
+    const selectedElement = document.querySelector(grabElement);
+    const newElement = document.createElement(appendElement);
+    selectedElement.append(newElement);
+    newElement.innerHTML = html;
+};
 
 
 //Refer to mockups and comments to dynamically show the info I grabbed from the API.
@@ -55,14 +92,32 @@ requestData(RandomUsersCall);
         4. City/Location
 */
 
-
-
+//go through array and for each user create the html. Will be called in a then so will accept data as a param
+const createGallery = (data) => {
+    console.log(data);
+    data.forEach(person => {
+        const galleryHTML = `<div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="${person.picture.medium}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+            <p class="card-text">${person.email}</p>
+            <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+            </div>
+        </div>`;
+        grabNappend('#gallery', 'div', galleryHTML);
+    });
+};
 
 //Exceeds - add the search box.
 //Exceeds - Add a way to filter the directory by name. 
 //Exceeds - Your search feature should filters results that are already on the page. So don't request new info from the API for your search.
 
+const createSearch = () => {
 
+
+};
 
 
 //Create a modal window
@@ -79,9 +134,16 @@ requestData(RandomUsersCall);
 //Make sure there is a way to close the window.
 //Exceeds - add a way to move to the next employee in the modal. There is markup and comments.
 
+const createModal = () => {
 
 
+};
 
+//make master function that creates the whole page by calling other functions?
+
+const createPage = () => {
+
+};
 
 /*
     Exceeds for Structure style and css
@@ -93,3 +155,18 @@ requestData(RandomUsersCall);
         2. Document your style changes in your readme and projec sub notes.
         3. Do not alter the layout or position of the important elements on the page.
 */
+
+/*
+    Event Listeners
+*/
+
+
+/*
+    Call functions.
+*/
+
+//Fetching the data from the RandomUser API
+requestData(RandomUsersCall)
+    .then(data => createGallery(data.results));
+
+    //place the functions here to test but then at the end only call the master createPage function.
